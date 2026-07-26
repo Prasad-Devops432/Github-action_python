bc2a4c81b26631e081695e009bc6dbafa279a0d9
Prasad <klprasad6@gmail.com>
2026-07-26 21:41:50 +1200

    github_actions

--- test_app.py
+++ test_app.py
@@ -1,6 +1,21 @@
-from app import app
+import pytest
+from app import app, read_requirements
 
-def test_homepage():
-    client = app.test_client()
-    response = client.get('/')
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_homepage(client):
+    response = client.get("/")
     assert response.status_code == 200
+    assert b"Hello" in response.data
+
+
+def test_read_requirements():
+    requirements = read_requirements("requirements.txt")
+    assert isinstance(requirements, list)
+    assert "flask" in requirements
